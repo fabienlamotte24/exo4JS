c28bf6f1150193ccc7eee737c2c05379811a7138
fabienlamotte24 <fabien.lamotte00@outlook.fr>
2018-06-20 11:39:16 +0200

    Exercice 4 JS

---
+++ assets/js/cours.js
@@ -0,0 +1,6 @@
+function display() {
+  var lastname = document.getElementById("lastname").value;
+  var firstname = document.getElementById("firstname").value;
+  var city = document.getElementById("city").value;
+alert("Nom: " + lastname + "\nPrenom: " + firstname + "\nVille: " + city);
+}
