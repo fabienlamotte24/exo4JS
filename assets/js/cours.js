1cb3a133e3dea5c035dee1f4d0be27a46946abb0
fabienlamotte24 <fabien.lamotte00@outlook.fr>
2018-06-21 09:59:24 +0200

    correction

--- assets/js/cours.js
+++ assets/js/cours.js
@@ -1,6 +1,14 @@
-function display() {
-  var lastname = document.getElementById("lastname").value;
-  var firstname = document.getElementById("firstname").value;
-  var city = document.getElementById("city").value;
-alert("Nom: " + lastname + "\nPrenom: " + firstname + "\nVille: " + city);
+/*Déclaration fonction identity*/
+function identity() {
+/*Déclaration des variable*/
+  var lastname = document.getElementById('lastname').value;
+  var firstname = document.getElementById('firstname').value;
+  var city = document.getElementById('city').value;
+  var regex = /^[a-zA-Z\-éèîç]+$/;
+  /*Boite de dialogue avec appel des variables*/
+  if((regex.test(lastname) == true) && (regex.test(firstname) == true) && (regex.test(city) == true)){
+    alert('Nom: ' + lastname + '\nPrenom: ' + firstname + '\nVille: ' + city);
+  }else{
+    alert('Recommencez avec des caractères corrects');
+  }
 }
